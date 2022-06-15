b10da124156ff19eeba2f34b1eaed8adce40a145
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-15 20:09:03 +0300

    Initial Commit

---
+++ TraficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLights
+//
+//  Created by Алиага С on 15.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
