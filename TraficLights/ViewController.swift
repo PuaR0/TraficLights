851c88594b49d648501d3a65e67e6dfce8cb5159
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-15 22:45:07 +0300

    Done

--- TraficLights/ViewController.swift
+++ TraficLights/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private let lightsOff: CGFloat = 0.5
+    private let lightsOn: CGFloat = 1
+    private var currentLight = CurrentLight.red
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("START", for: .normal)
+        
+        redLightView.backgroundColor = .red
+        yellowLightView.backgroundColor = .yellow
+        greenLightView.backgroundColor = .green
+        
+        redLightView.alpha = lightsOff
+        yellowLightView.alpha = lightsOff
+        greenLightView.alpha = lightsOff
+ 
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightsOff
+            redLightView.alpha = lightsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightsOff
+            yellowLightView.alpha = lightsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightsOff
+            greenLightView.alpha = lightsOn
+            currentLight = .red
+        }
+    }
 }
 
